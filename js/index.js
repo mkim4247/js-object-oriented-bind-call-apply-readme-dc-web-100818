ea28223a07788c47f1b39c23868fb1c6cb1d8334
Michael Kim <m.kim4247@gmail.com>
2020-06-08 21:02:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+// use this to copy code snippets or us your browser's console
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
- 
-greet(); // my name is , hi!
- 
-let person = {
-    name: 'Bob',
-    greet: greet
-};
- 
-person.greet(); // my name is Bob, hi!
+
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
